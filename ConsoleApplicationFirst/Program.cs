552db9a44711e389f8494f0751289087972fbdbb
Student <Student@FJPC-18>
2016-11-01 14:51:50 +0200

    Add project files.

---
+++ ConsoleApplicationFirst/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime date = DateTime.Now;
+            date = date.AddDays(10);
+            Console.WriteLine("ALOOO");
+            Console.WriteLine("Today + 10 is {0:d} at {0:T}", date) ;
+            Console.WriteLine($"Today + 10 is {date.ToShortDateString()} at {date.ToShortTimeString()}");
+        }
+    }
+}
